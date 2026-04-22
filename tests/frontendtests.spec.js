0d2e76eb9917868196b72d02fbf1533c545c9b7d
beans79 <65426351+beans79@users.noreply.github.com>
2026-04-22 21:45:03 +0100

    primeiro teste de login no FrontEnd

--- tests/frontendtests.spec.js
+++ tests/frontendtests.spec.js
@@ -1,1 +1,16 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
+
+//////teste do login//// ver se funciona o login com email e senha corretos, e se aparece a mensagem de sucesso, e se o objeto usuario é retornado sem a senha
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/login.html');
+  await page.getByRole('textbox', { name: 'Email:' }).click();
+  await page.getByRole('textbox', { name: 'Email:' }).fill('admin@biblioteca.com');
+  await page.getByRole('textbox', { name: 'Senha:' }).click();
+  await page.getByRole('textbox', { name: 'Senha:' }).fill('123456');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Entrar' }).click();
+});
