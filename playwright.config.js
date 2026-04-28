14da1948946f764ef37fd0e378bab9baf0cb8b97
beans79 <65426351+beans79@users.noreply.github.com>
2026-04-28 20:36:25 +0100

    Acrescento de configuração no ficheiro playwright.config.js para definir a baseURL para os testes.

--- playwright.config.js
+++ playwright.config.js
@@ -77,5 +77,8 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  //use: {
+  //  baseURL: 'http://127.0.0.1:3000',
+  //},
 });
 
